221ef1381512093157076710f6dbe56a1fa00b92
ghjumi2222 <57886708+ghjumi2222@users.noreply.github.com>
2019-12-19 16:03:31 +0900

    pf5

--- pf5/js/index.js
+++ pf5/js/index.js
@@ -1,50 +1,69 @@
-//조작은 중간과정이 안보입니다.
-//동작은 중간과정이 보입니다.
-
-setInterval("auto_gellery()",8000);
-function auto_gellery(){
-   $(".vs_box").stop().animate({marginLeft: "-100%"},500,function(){ //동작
-      $(".vs_box li:first").appendTo(".vs_box");//조작
-      $(".vs_box").css({marginLeft:0});//조작
-   });
-};
-
-
 $(document).ready(function(){
-        var wd = parseInt($(window).width());
-        var aniWid = wd*-1;
-        var cnt = parseInt($(".level_img li").length);
-        $("#vdo1").width($(window).width());
-        $("#vdo1").height($(window).width()/4*3);
-        var intv = setInterval(function(){ leftAni(); },2000);
-        $(".btn_box .nex").click(function () {
-            clearInterval(intv);
-            leftAni();
-            intv = setInterval(function(){ leftAni(); }, 2000);
+    
+    var intv = setInterval(function(){
+        autoAni();
+    },2800);
+    var vswd=parseInt($('.vs_box li').width());
+    function autoAni(){
+        $('.vs_box').not(':animated').animate({
+            marginLeft:'-'+vswd+'px'
+        },1800,function(){
+            
+            $('.vs_box').css('margin-left','0px');
+            $('.vs_box li').eq(0).appendTo($('.vs_box'));
+            
+            
         });
-        $(".btn_box .pre").click(function () {
-            clearInterval(intv);
-            rightAni();
-            intv = setInterval(function(){ leftAni(); }, 2000);
+        
+        
+        
+    };
+    
+    
+    var intv2=setInterval(function(){
+        nexAni()
+    },2800);
+    
+    
+    var level_wd=parseInt($('.level_img li').width());
+    
+    function nexAni(){
+        $('.level_img').not(':animated').animate({
+            marginLeft:'-'+level_wd+'px'
+        },800,function(){
+            
+            $('.level_img').css('margin-left','0px');
+            $('.level_img li').eq(0).appendTo($('.level_img'));
+            
+            
         });
-        function rightAni() {
-            $(".level_img li").eq(2).prependTo(".level_img");
-            $(".level_img").css("margin-left", aniWid);
-            $(".level_img").animate({"margin-left": "0px"}, 800);                
-        };
-        function leftAni() {
-            $(".level_img").animate({"margin-left" : aniWid}, 800, function(){
-                $(".level_img li").eq(0).appendTo(".level_img");
-                $(".level_img").css("margin-left", "0px");
-            });
-        };
-    })
-
-jQuery(function($){
-	$(".nav2 li.item2").click(function(){
-		$('.nav').stop().slideToggle('fast');
-	});
+        
+    };
+    function priAni(){
+        $('.level_img li').eq(4).prependTo( $('.level_img'));
+        
+        $('.level_img').css('margin-left','-'+level_wd+'px');
+         $('.level_img').animate({marginLeft:'0px'},800)
+        
+    };
+    
+    $('.nex').click(function(){
+        clearInterval(intv2)
+        nexAni();
+        intv2=setInterval(function(){
+        nexAni()
+    },2800);
+        
+    });
+    
+    $('.pre').click(function(){
+        clearInterval(intv2)
+        priAni();
+        intv2=setInterval(function(){
+        nexAni()
+    },2800);
+    });
+    
+    
+    
 });
-
-
-
